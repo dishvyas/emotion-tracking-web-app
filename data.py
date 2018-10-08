26c4e9b002419c601f34a57975033f2ca4f7d8e4
m4sum <yashjani@Yashs-MacBook-Pro.local>
2018-10-08 22:29:09 +0530

    Web apps home page and srticle pages made

---
+++ data.py
@@ -0,0 +1,36 @@
+def Articles():
+    articles = [
+
+        {
+            'id':1,
+            'title':'Article one',
+            'body': 'You can also use pdb, pudb, or another terminal debugger by calling set_trace in the view where you want to start debugging.',
+            'author': 'jack ma',
+            'createDate': '5/10',
+        },
+
+        {
+            'id':2,
+            'title':'Article two',
+            'body': 'You can also use pdb, pudb, or another terminal debugger by calling set_trace in the view where you want to start debugging.',
+            'author': 'jack ma',
+            'createDate': '5/10',
+        },
+
+        {
+            'id': 3,
+            'title': 'Article three',
+            'body': 'You can also use pdb, pudb, or another terminal debugger by calling set_trace in the view where you want to start debugging.',
+            'author': 'jack ma',
+            'createDate': '5/10',
+        },
+
+        {
+            'id': 4,
+            'title': 'Article four',
+            'body': 'You can also use pdb, pudb, or another terminal debugger by calling set_trace in the view where you want to start debugging.',
+            'author': 'jack ma',
+            'createDate': '5/10',
+        },
+    ]
+    return articles
