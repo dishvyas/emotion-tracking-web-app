31537deb9e3f2907c1ad2cd0dc81a1365d8ddfc2
Dishant <dishant.vyas93@gmail.com>
2019-09-07 01:31:19 +0530

    First Commit

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from flask import Response
+from camera import VideoCamera
 
 app=Flask(__name__)
 
@@ -11,6 +13,17 @@ Articles = Articles()
 @app.route('/')
 def index():
     return render_template('home.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/about')
 def about():
