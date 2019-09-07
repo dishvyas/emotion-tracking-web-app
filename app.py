91ffebc1b38fe9a3420b9a0488f8db91952081e7
nish17 <nimesh3536@gmail.com>
2019-09-07 07:12:10 +0530

    update UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,flash, redirect, url_for, session, request, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, PasswordField, validators
@@ -6,13 +6,15 @@ from passlib.hash import sha256_crypt
 from flask import Response
 from camera import VideoCamera
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 Articles = Articles()
+
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
 
 def gen(camera):
     while True:
@@ -20,26 +22,37 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
+
+@app.route("/login/", methods=['POST'])
+def move_forward():
+    forward_message = "Moving Forward..."
+    return render_template('afterLogin.html', forward_message=forward_message)
+
+
+@app.route('/video_feed', methods=['GET'])
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-    
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/articles')
 def articles():
     return render_template('articles.html', articles=Articles)
 
+
 class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1 , max=50)])
+    name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email', [validators.Length(min=6 , max=50)])
-    password = PasswordField('Password', [validators.DataRequired(), validators.EqualTo('confirm', message='Passwords do not match')])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [validators.DataRequired(
+    ), validators.EqualTo('confirm', message='Passwords do not match')])
     confirm = PasswordField('Confirm Password')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -48,9 +61,10 @@ def register():
         return render_template('register.html')
     return render_template('register.html', form=form)
 
-#@app.route('/articles/<string:id>/')
-#def article(id):
+# @app.route('/articles/<string:id>/')
+# def article(id):
  #   return render_template('article.html', id=id)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug='true')
